0c16e3378a8e4a6645790611e9aa6636a96d532f
devMohamedNasser <m.nasser.dev@gmail.com>
2025-09-15 22:41:33 +0300

    Changes in token for production

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -3,7 +3,8 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-    const encodedToken = (await cookies()).get("next-auth.session-token")?.value; 
+  const cookieStore = await cookies();
+    const encodedToken = cookieStore.get('next-auth.session-token')?.value || cookieStore.get("__Secure-next-auth.session-token")?.value; 
     if (!encodedToken) 
       return null;
 
